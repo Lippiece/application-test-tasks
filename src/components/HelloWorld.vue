0cf7a15bc711160106cdc6babba3ca60ccdaaba5
lippiece <github@lippiece.anonaddy.me>
2025-08-20 15:19:01 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">
+      {{ msg }}
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
+
