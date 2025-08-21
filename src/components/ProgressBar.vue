fdaa2b84e70b7c3c27c9156b8b063a6fa003e641
lippiece <github@lippiece.anonaddy.me>
2025-08-22 00:28:20 +0200

    feat(DimaTech): add "dashboard" progress bar type

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, shallowRef } from "vue"
+import { computed, ref, shallowRef, useTemplateRef } from "vue"
 
 import IconError from "./icons/IconError.vue"
 import IconSuccess from "./icons/IconSuccess.vue"
@@ -28,12 +28,18 @@ const statusIcons = shallowRef({
 
 const progress = defineModel<string>({ default: "0" })
 
-const radius        = ref("90")
-const circumference = computed(() => 2 * Math.PI * Number(radius.value))
-const visible       = computed(
-  () => circumference.value - (60 / 360) * circumference.value,
+const progressPercent = computed(() => Number(progress.value) / 100)
+
+const defaultBackground = useTemplateRef<SVGCircleElement>("default-background")
+const radius            = ref(90)
+const gapAngle          = ref(60)
+const circumference     = computed(
+  () => defaultBackground.value?.getTotalLength() || 0,
 )
-const gap           = computed(() => (60 / 360) * circumference.value)
+const visible           = computed(
+  () => circumference.value - (gapAngle.value / 360) * circumference.value,
+)
+const gap = computed(() => (gapAngle.value / 360) * circumference.value)
 </script>
 
 <template>
@@ -54,7 +60,9 @@ const gap           = computed(() => (60 / 360) * circumference.value)
     />
 
     <svg
-      class="-rotate-90"
+      :class="{
+        '-rotate-90': type === 'default',
+      }"
       width="200"
       height="200"
       viewBox="-25 -25 250 250"
@@ -63,26 +71,63 @@ const gap           = computed(() => (60 / 360) * circumference.value)
     >
 
       <circle
-        class="stroke-zinc-200 dark:stroke-zinc-800"
+        ref="default-background"
+        :class="{
+          'stroke-zinc-200 dark:stroke-zinc-800': type === 'default',
+          'translate-y-[200px] -rotate-90 stroke-zinc-200 dark:stroke-zinc-800':
+            type === 'dashboard',
+        }"
         :r="radius"
         cx="100"
         cy="100"
         fill="transparent"
         stroke-width="16px"
+        stroke-linecap="round"
+        :stroke-dasharray="type === 'dashboard' ? `${visible} ${gap}` : ''"
+        :stroke-dashoffset="
+          type === 'dashboard' ? circumference / 2 - gap / 2 : ''
+        "
       />
 
       <circle
         v-if="type === 'default'"
         class="transition-all"
-        r="90"
+        cx="100"
+        cy="100"
+        stroke-width="16px"
+        stroke-linecap="round"
+        fill="transparent"
+        :r="radius"
+        :stroke="progressStroke"
+        :stroke-dashoffset="circumference * (1 - progressPercent)"
+        :stroke-dasharray="circumference"
+      />
+
+    </svg>
+
+    <svg
+      v-if="type === 'dashboard'"
+      class="absolute top-0 left-0 rotate-120"
+      width="200"
+      height="200"
+      viewBox="-25 -25 250 250"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+
+      <circle
+        class="transition-all"
+        :r="radius"
         cx="100"
         cy="100"
         :stroke="progressStroke"
         stroke-width="16px"
         stroke-linecap="round"
-        :stroke-dashoffset="2 * Math.PI * 90 * (1 - Number(progress) / 100)"
-        fill="transparent"
+        :stroke-dashoffset="
+          circumference * (1 - progressPercent * (1 - gapAngle / 360))
+        "
         :stroke-dasharray="circumference"
+        fill="transparent"
       />
 
     </svg>
